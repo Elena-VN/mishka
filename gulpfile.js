2efee7e106f62142d895c84df83fa75dbe08b2f0
Lena-VN <second68@gmail.com>
2018-11-23 14:41:30 +0300

    исправлен отступ

--- gulpfile.js
+++ gulpfile.js
@@ -1,43 +1,26 @@
-// const less = require("gulp-less");
-// const gulp = require("gulp");
-// const autoprefixer = require("gulp-autoprefixer");
-//
-// gulp.task("less", function () {
-//   return gulp.src("source/less/*.less")
-//     .pipe(less())
-//     .pipe(gulp.dest("source/css"));
-// });
-//
-// gulp.task("default", () =>
-//   gulp.src("source/css/*.css")
-//     .pipe(autoprefixer({
-//       browsers: ["last 2 versions"],
-//       cascade: false
-//     }))
-//     .pipe(gulp.dest("dist"))
-// );
-const gulp       = require("gulp"), // Подключаем Gulp
-  less         = require("gulp-less"), //Подключаем Sass пакет,
-  browserSync  = require("browser-sync"), // Подключаем Browser Sync
-  concat       = require("gulp-concat"), // Подключаем gulp-concat (для конкатенации файлов)
-  uglify       = require("gulp-uglifyjs"), // Подключаем gulp-uglifyjs (для сжатия JS)
-  cssnano      = require("gulp-cssnano"), // Подключаем пакет для минификации CSS
-  rename       = require("gulp-rename"), // Подключаем библиотеку для переименования файлов
-  del          = require("del"), // Подключаем библиотеку для удаления файлов и папок
-  imagemin     = require("gulp-imagemin"), // Подключаем библиотеку для работы с изображениями
-  pngquant     = require("imagemin-pngquant"), // Подключаем библиотеку для работы с png
-  cache        = require("gulp-cache"), // Подключаем библиотеку кеширования
-  autoprefixer = require("gulp-autoprefixer");// Подключаем библиотеку для автоматического добавления префиксов
+let gulp = require("gulp"), // Подключаем Gulp
+  less = require("gulp-less"), //Подключаем less пакет,
+  browserSync = require("browser-sync"), // Подключаем Browser Sync
+  concat = require("gulp-concat"), // Подключаем gulp-concat (для конкатенации файлов)
+  uglify = require("gulp-uglifyjs"), // Подключаем gulp-uglifyjs (для сжатия JS)
+  cssnano = require("gulp-cssnano"), // Подключаем пакет для минификации CSS
+  rename = require("gulp-rename"), // Подключаем библиотеку для переименования файлов
+  del = require("del"), // Подключаем библиотеку для удаления файлов и папок
+  imagemin = require("gulp-imagemin"), // Подключаем библиотеку для работы с изображениями
+  pngquant = require("imagemin-pngquant"), // Подключаем библиотеку для работы с png
+  cache = require("gulp-cache"), // Подключаем библиотеку кеширования
+  autoprefixer = require("gulp-autoprefixer"); // Подключаем библиотеку для автоматического добавления префиксов
 
-gulp.task("less", function(){ // Создаем таск Sass
-  return gulp.src("source/less/**/*.less") // Берем источник
-    .pipe(less()) // Преобразуем Sass в CSS посредством gulp-less
-    .pipe(autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], { cascade: true })) // Создаем префиксы
+
+gulp.task("less", function () { // Создаем таск less
+  return gulp.src("source/less/style.less") // Берем источник
+    .pipe(less()) // Преобразуем less в CSS посредством gulp-less
+    .pipe(autoprefixer(["last 15 versions", "> 1%", "ie 8", "ie 7"], {cascade: true})) // Создаем префиксы
     .pipe(gulp.dest("source/css")) // Выгружаем результата в папку source/css
-    .pipe(browserSync.reload({stream: true})) // Обновляем CSS на странице при изменении
+    .pipe(browserSync.reload({stream: true})); // Обновляем CSS на странице при изменении
 });
 
-gulp.task("browser-sync", function() { // Создаем таск browser-sync
+gulp.task("browser-sync", function () { // Создаем таск browser-sync
   browserSync({ // Выполняем browserSync
     server: { // Определяем параметры сервера
       baseDir: "source" // Директория для сервера - source
@@ -46,7 +29,7 @@ gulp.task("browser-sync", function() { // Создаем таск browser-sync
   });
 });
 
-gulp.task("scripts", function() {
+gulp.task("scripts", function () {
   return gulp.src([ // Берем все необходимые библиотеки
     "source/libs/jquery/dist/jquery.min.js", // Берем jQuery
     "source/libs/magnific-popup/dist/jquery.magnific-popup.min.js" // Берем Magnific Popup
@@ -56,24 +39,24 @@ gulp.task("scripts", function() {
     .pipe(gulp.dest("source/js")); // Выгружаем в папку source/js
 });
 
-gulp.task("css-libs", ["less"], function() {
+gulp.task("css-libs", ["less"], function () {
   return gulp.src("source/css/libs.css") // Выбираем файл для минификации
     .pipe(cssnano()) // Сжимаем
     .pipe(rename({suffix: ".min"})) // Добавляем суффикс .min
     .pipe(gulp.dest("source/css")); // Выгружаем в папку source/css
 });
 
-gulp.task("watch", ["browser-sync", "css-libs", "scripts"], function() {
+gulp.task("watch", ["browser-sync", "css-libs", "scripts"], function () {
   gulp.watch("source/less/**/*.less", ["less"]); // Наблюдение за less файлами в папке less
   gulp.watch("source/*.html", browserSync.reload); // Наблюдение за HTML файлами в корне проекта
   gulp.watch("source/js/**/*.js", browserSync.reload);   // Наблюдение за JS файлами в папке js
 });
 
-gulp.task("clean", function() {
+gulp.task("clean", function () {
   return del.sync("dist"); // Удаляем папку dist перед сборкой
 });
 
-gulp.task("img", function() {
+gulp.task("img", function () {
   return gulp.src("source/img/**/*") // Берем все изображения из source
     .pipe(cache(imagemin({  // Сжимаем их с наилучшими настройками с учетом кеширования
       interlaced: true,
